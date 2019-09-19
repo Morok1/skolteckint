e3f3b53cdd064f7d92e09f15df065bdb863dc699
zhenyasmagin@yandex.ru <zhenyasmagin@yandex.ru>
2019-09-19 11:27:18 +0300

    remove unused

--- src/main/java/com/smagin/skolteckint/skolteckint/controller/StatementController.java
+++ src/main/java/com/smagin/skolteckint/skolteckint/controller/StatementController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Date;
